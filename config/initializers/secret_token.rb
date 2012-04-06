1112c42d25f7e343317068a2a87af322eeeee236
Briana Fulfer <blfulfer@csu.fullerton.edu>
2012-04-06 08:56:17 -0700

    Initial Comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoundfileApp::Application.config.secret_token = '44f8ee39654e851b8cf1b17c0e4b7631367f73c871b35d356021272a5381a8e27ec8b59d04ba994d25758b6936ae1491bc77465f2ec7d61ce0825547d8115f67'
